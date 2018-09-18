dfec2dcbfd5709b001d023e8c99620b0d99f9f8f
dyuki <unblivion@gmail.com>
2018-09-17 23:05:11 -0300

    80% it's working

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+let chooseSide = document.querySelector("#chooseside");
+let sabers = document.querySelector("#sabers");
+let buttonJedi = document.querySelector(".jedi");
+let buttonSith = document.querySelector(".sith");
+let lightSaber = document.querySelector(".tuim");
+
+function escolherSith() {
+    chooseSide.style.display = 'none';
+    sabers.style.display = 'flex';
+    lightSaber.style.background = 'linear-gradient(90deg, #A50D08, red, #A50D08'
+    lightSaber.style.boxShadow = '0 0 50px red';
+}
+
+function escolherJedi() {
+    chooseSide.style.display = 'none';
+    sabers.style.display = 'flex';
+}
+
+function turnOn(){
+    if(lightSaber.style.opacity == 1){
+        lightSaber.style.opacity = 0;
+    }
+    else{
+        lightSaber.style.opacity = 1;
+    }
+}
+
+buttonSith.onclick = escolherSith;
+buttonJedi.onclick = escolherJedi;
+sabers.onclick = turnOn;
+
 // Registra o service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
